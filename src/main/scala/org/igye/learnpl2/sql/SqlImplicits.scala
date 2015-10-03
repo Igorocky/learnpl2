a92d41053dbf1ff38189fe1c14a472990bd89435
Igorocky <Igorocky@users.noreply.github.com>
2015-10-04 00:41:15 +0200

    Remove SQL

--- src/main/scala/org/igye/learnpl2/sql/SqlImplicits.scala
+++
@@ -1,7 +0,0 @@
-package org.igye.learnpl2.sql
-
-trait SqlImplicits {
-    def fieldOfTableAlias(aliasStr: String, field: Field): RefToField = {
-        new RefToField(new FieldAlias(new TableAlias(field.table, aliasStr), field))
-    }
-}
