0b4d48d64b55e14442ce2328e648bccf1603c5d8
Igorocky <Igorocky@users.noreply.github.com>
2016-01-23 12:18:14 +0100

    Introduce a constant in LoadTextModelImpl.scala

--- src/main/scala/org/igye/learnpl2/models/impl/LoadTextModelImpl.scala
+++ src/main/scala/org/igye/learnpl2/models/impl/LoadTextModelImpl.scala
@@ -7,18 +7,20 @@ import org.apache.commons.io.FileUtils
 import org.igye.learnpl2.models.LoadTextModel
 
 class LoadTextModelImpl extends LoadTextModel {
+    private final val ENCODING = "UTF-8"
+
     val text = new SimpleStringProperty()
     override val loadedFrom: StringProperty = new SimpleStringProperty()
     override val caretPosition: IntegerProperty = new SimpleIntegerProperty()
 
     override def loadFromFile(filePath: String): Unit = {
-        text.set(FileUtils.readFileToString(new File(filePath), "UTF-8"))
+        text.set(FileUtils.readFileToString(new File(filePath), ENCODING))
         loadedFrom.set(filePath)
     }
 
     override def saveAs(filePath: String): Unit = {
         loadedFrom.set(filePath)
-        FileUtils.writeStringToFile(new File(filePath), text.get(), "UTF-8")
+        FileUtils.writeStringToFile(new File(filePath), text.get(), ENCODING)
     }
 
     override def save(): Unit = {
